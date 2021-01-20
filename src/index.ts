27dd8a9c429744fb98c460103afe8cd067a3ef87
Gunil <gunil.na1@gmail.com>
2021-01-21 06:32:15 +0900

    showing popup windows

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import { CustomMap } from './CustomMap';
 const user = new User();
 const customMap = new CustomMap('map');
 const company = new Company();
+
 customMap.addMarker(user);
 customMap.addMarker(company);
